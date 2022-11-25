8d124430c6c0b4f62ee9f616bc6c532e30e411ee
Owlaboy <andrei.preotesa.ap@gmail.com>
2022-11-25 15:44:44 +0200

    added test for checking the right amount of purchases when one product is added to the basket

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,11 +3,11 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self.ostoslista = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostokset)
+        return len(self.ostoslista)
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
@@ -15,15 +15,15 @@ class Ostoskori:
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         hinta = 0
-        if self.ostokset !=[]:
-            for ostos in self.ostokset:
+        if self.ostoslista !=[]:
+            for ostos in self.ostoslista:
                 hinta += ostos.hinta()
         return hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
         uusi_ostos = Ostos(lisattava)
-        self.ostokset.append(uusi_ostos)
+        self.ostoslista.append(uusi_ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -34,6 +34,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self.ostokset
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
