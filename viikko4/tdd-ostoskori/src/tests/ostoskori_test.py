86c4619a2cce9c9c2150cdab1d0b18d0bc8befee
Owlaboy <andrei.preotesa.ap@gmail.com>
2022-11-25 16:24:42 +0200

    formattin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -41,7 +41,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(water)
 
         self.assertEqual(self.kori.hinta(), 99999+1)
-        
+
+    #test6
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
         water = Tuote("water", 1)
         self.kori.lisaa_tuote(water)
@@ -49,6 +50,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
     
+    #test7
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_hinta_on_sama_kuin_2_kertaa_tuotteen_hinta(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
@@ -56,6 +58,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.hinta(), 50*2)
 
+    #test8
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_1_ostos(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
@@ -63,12 +66,14 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(maara, 1)
 
+    #test9
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_oikean_ostoksen(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
 
         self.assertEqual(self.kori.ostokset()[0].tuote, water)
 
+    #test10
     def test_kahden_tuottee_lisaamisen_jalkeen_ostoskori_sisataa_kaksi_ostosta(self):
         water = Tuote("fiji water", 50)
         truffel_oil = Tuote("truffle oil" , 99999)
@@ -77,6 +82,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(len(self.kori.ostokset()), 2)
 
+    #test11
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_yhden_ostoksen(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
@@ -84,6 +90,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(len(self.kori.ostokset()), 1)
 
+    #test12
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_oikean_laisen_ostoksen(self):
         water = Tuote("fiji water", 50)
         
@@ -93,6 +100,7 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.ostokset()[0].tuotteen_nimi(), "fiji water")
         self.assertEqual(self.kori.ostokset()[0].lukumaara(), 2)
 
+    #test13
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_poistettua_yksi_tuote_ostoskori_sisaltaa_yhden_ostoksen(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
@@ -101,6 +109,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.ostokset()[0].lukumaara(), 1)
 
+    #test14
     def test_viimeisen_tuotteen_poistamisen_jalkeen_korissa_on_0_ostosta(self):
         water = Tuote("fiji water", 50)
         self.kori.lisaa_tuote(water)
@@ -108,6 +117,7 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(len(self.kori.ostokset()), 0)
     
+    #test15
     def test_kori_voidaan_tyhjentaa(self):
         booleanitem = Tuote("totta pöriset", 1)
         self.kori.lisaa_tuote(booleanitem)
